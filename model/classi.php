43c654e3dbdbc7cf30a21232ff36093babe14f63
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-03-08 14:38:05 +0100

    Creato tre classi e inserito attributi

--- model/classi.php
+++ model/classi.php
@@ -1,6 +1,37 @@
 <?php 
 
+class ProductType {
+    public $name;
 
+    public function __construct($_name) {
+        $this->name = $_name;
+    }
+}
+
+
+class Category {
+    public $animal;
+    public $icon;
+
+    public function __construct($_animal, $_icon) {
+        $this->animal = $_animal;
+        $this->icon = $_icon;
+    }
+}
+
+class Product {
+    public $title;
+    public $price;
+    public $image;
+    public $category;
+
+    public function __construct($_title, $_price, $_image, Category $_category) {
+        $this->title = $_title;
+        $this->price = $_price;
+        $this->image = $_image;
+        $this->category = $_category;
+    }
+}
 
 
 ?>
